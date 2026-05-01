873c95594b3ff13ebc12584aa2f042dbf59da783
Neonberry444 <a_toktarbai@kbtu.kz>
2026-05-01 15:16:37 +0500

    Add files via upload

---
+++ Practice1/python-basics/Variables.py
@@ -0,0 +1,27 @@
+#1.
+x = 5
+y = "John"
+print(x)
+print(y)
+#2.
+myvar = "John"
+my_var = "John"
+_my_var = "John"
+myVar = "John"
+MYVAR = "John"
+myvar2 = "John"
+#3.
+x, y, z = "Orange", "Banana", "Cherry"
+print(x)
+print(y)
+print(z)
+#4.
+x = "Python is awesome"
+print(x)
+#5.
+x = "awesome"
+
+def myfunc():
+  print("Python is " + x)
+
+myfunc()
